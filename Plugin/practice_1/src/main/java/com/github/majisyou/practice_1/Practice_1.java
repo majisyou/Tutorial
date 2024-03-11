86b72b4f7b632a1a6a0dfdcb5b4461ea92a62d4b
majisyou <majisyou1999@gmail.com>
2024-03-12 02:57:08 +0900

    init commit

---
+++ Plugin/practice_1/src/main/java/com/github/majisyou/practice_1/Practice_1.java
@@ -0,0 +1,17 @@
+package com.github.majisyou.practice_1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Practice_1 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
