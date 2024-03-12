48f146c28582d8909a690fc273e94a4b92648092
majisyou <majisyou1999@gmail.com>
2024-03-12 15:40:15 +0900

    Question and Answer

---
+++ Plugin/practice_2/src/main/java/com/github/majisyou/practice_2/Practice_2Event.java
@@ -0,0 +1,21 @@
+package com.github.majisyou.practice_2;
+
+import org.bukkit.entity.Cow;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+public class Practice_2Event implements Listener {
+
+    /**
+     * これは牛を右クリックしたら爆発させるイベント
+     * 変数：PlayerInteractEntityEvent event
+     * 結果：void
+     * */
+    @EventHandler
+    public void onEntityRightClick(PlayerInteractEntityEvent event){
+        if(event.getRightClicked() instanceof Cow cow){
+            cow.getWorld().createExplosion(cow.getLocation(),2.5F);
+        }
+    }
+}
